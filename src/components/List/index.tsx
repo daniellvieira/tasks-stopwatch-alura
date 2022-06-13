bf296ab843f6011df3e5a102c9327b11a33be97a
Daniel Fernandes Vieira <danielvieiraufc@gmail.com>
2022-06-13 20:40:05 -0300

    feat: useState in List component

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import style from './List.module.scss';
 import Item from "./Item";
 
 function List() {
-  const tasks = [{
+  const tasksDefault = [{
     name: 'React',
     time: '02:00:00'
   }, {
@@ -12,11 +12,14 @@ function List() {
   }, {
     name: 'Typescript',
     time: '03:00:00'
-  }]
+  }];
+  const [tasks, setTasks] = useState(tasksDefault);
 
   return(
     <aside className={style.listTasks}>
-      <h2>Estudos do dia</h2>
+      <h2 onClick={() => {
+        setTasks([...tasks, { name: 'Daniel', time: '05:00:00' }])
+      }}>Estudos do dia</h2>
       <ul>
         { tasks.map((task, index) => (
           <Item key={index} {...task} />
